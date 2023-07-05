7131108d0504f4198f559de7b9766ba2a5274ffc
Mahziar MirAzimi <seyedmahziar59@gmail.com>
2023-07-06 03:13:31 +0330

    Iperson interface

---
+++ DataAccess/Models/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+	public interface IPerson
+	{
+		string FirstName { get; set; }
+		string LastName { get; set; }
+		string Email { get; set; }
+		string Username { get; set; }
+		string Password { get; set; }
+	}
+}
