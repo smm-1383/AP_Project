e1aa86905f84f8bed6603e7b829852918793664e
Mahziar MirAzimi <seyedmahziar59@gmail.com>
2023-07-06 03:21:16 +0330

    Customer model

---
+++ DataAccess/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+	public class Customer:IPerson
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int ID { get; set; }
+		public string Email { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Phone { get; set; }
+		public double Wallet { get; set; }
+	}
+}
