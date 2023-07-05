51cd487331ca82ea93d01b6cbce20e72a0247f99
Mahziar MirAzimi <seyedmahziar59@gmail.com>
2023-07-06 03:15:41 +0330

    employee model

---
+++ DataAccess/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+	public class Employee:IPerson
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Personal_ID { get; set; }
+		public string Email { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
